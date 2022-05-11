f50631b651d090fbbacf6ec39d4946a5514bf025
karensand <ksilva@mtsolucoes.com.br>
2022-05-11 08:25:18 -0300

    Alterações Responsividade

--- SA2 - Game Mania/js/script.js
+++ SA2 - Game Mania/js/script.js
@@ -1,36 +1,27 @@
-$(document).ready(function()){
-    $("#hamburguer").click(function(){
-        $("#menu").toggleClass("#menu-ativo")
-    })
-})
+$( document ).ready(function() {
 
-
-// $('.carrosel').slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 4,
-//     slidesToScroll: 4,
-//   });
-
-
-
-$(document).ready(function() {
     $("#carrossel img:eq(0)").addClass("banner-ativo").show()
 
+
+    setInterval(slide, 2000) 
+
+    function slide(){
+        if($(".banner-ativo").next().length ){
+            $(".banner-ativo").removeClass("banner-ativo").hide().next().addClass("banner-ativo").show()
+        }else{
+            $(".banner-ativo").removeClass().hide()
+            $("#carrosel img:eq(0)").addClass("banner-ativo").show()
+        }
+    }   
+
 })
 
-setInterval(slide,1000);
+$("#hamburguer").click(function () {
+    $("#menu").toggleClass("menu-ativo")
+})
 
-function slide(){
-    if($(".banner-ativo").next().length){
-        $(".banner-ativo").removeClass("banner-ativo").hide().next().addClass("banner-ativo").show()
-    }else{
-        $(".banner-ativo").removeClass().hide()
-        $("#carrossel img:eq(0)").addClass("banner-ativo").show()
-    }
-}
 
+  
 //     $("#hamburguer").click(function(){
 //         if($("#menu").hasClass("#menu-ativo")){
 //             $("#menu").removeClass("#menu-ativo")
@@ -47,92 +38,99 @@ function slide(){
 
 let email = document.getElementById("campo-email");
 
-function enviarEmail(){
+// function enviarEmail(){
+//     let emailDigitado = email.value;
+//     console.log(emailDigitado)
+// }
+
+let listaProdutos = [
+    {
+        titulo: "God of War",
+        descricao: "R$10,00"
+    },
+    {
+        titulo: "PSE 2021",
+        descricao: "R$15,00" 
+    },
+    {
+        titulo: "Fida 2022",
+        descricao: "R$20,00" 
+    },
+    {
+        titulo: "Nabunagasr",
+        descricao: "R$10,00" 
+    },
+    {
+        titulo: "Kit teclado",
+        descricao: "R$150,00" 
+    },
+    {
+        titulo: "Acessorios",
+        descricao: "R$100,00" 
+    },
+    {
+        titulo: "Cadeira",
+        descricao: "R$350,00" 
+    },
+    {
+        titulo: "Suporte para celula",
+        descricao: "R$50,00" 
+    },
+    {
+        titulo: "Assasinos Creed Valhalla",
+        descricao: "R$10,00" 
+    },
+    {
+        titulo: "Assasinos Creed III",
+        descricao: "R$10,00" 
+    },
+    {
+        titulo: "The Witcher",
+        descricao: "R$25,00" 
+    },
+    {
+        titulo: "Velozes e Furiosos Encruzilhada",
+        descricao: "R$30,00" 
+    },
+    {
+        titulo: "The Force Unleashed",
+        descricao: "R$10,00" 
+    },
+    {
+        titulo: "The Sky Walker Saga",
+        descricao: "R$10,00" 
+    },
+    {
+        titulo: "Star Wars III",
+        descricao: "R$10,00" 
+    },
+    {
+        titulo: "Old Republic",
+        descricao: "R$10,00" 
+    },
+]
+
+function enviarEmail() {
     let emailDigitado = email.value;
     console.log(emailDigitado)
 }
 
-// let listaProdutos = [
-//     {
-//         titulo:"God of War"
-//         Descricao:"R$10,00"
-//     },
-//     {
-//         titulo:"PSE 2021"
-//         Descricao:"R$15,00" 
-//     },
-//     {
-//         titulo:"Fida 2022"
-//         Descricao:"R$20,00" 
-//     },
-//     {
-//         titulo:"Nabunagasr"
-//         Descricao:"R$10,00" 
-//     },
-//     {
-//         titulo:"Kit teclado"
-//         Descricao:"R$150,00" 
-//     },
-//     {
-//         titulo:"Acessorios"
-//         Descricao:"R$100,00" 
-//     },
-//     {
-//         titulo:"Cadeira"
-//         Descricao:"R$350,00" 
-//     },
-//     {
-//         titulo:"Suporte para celula"
-//         Descricao:"R$50,00" 
-//     }
-//     {
-//         titulo:"Assasinos Creed Valhalla"
-//         Descricao:"R$10,00" 
-//     },
-//     {
-//         titulo:"Assasinos Creed III"
-//         Descricao:"R$10,00" 
-//     },
-//     {
-//         titulo:"The Witcher"
-//         Descricao:"R$25,00" 
-//     },
-//     {
-//         titulo:"Velozes e Furiosos Encruzilhada"
-//         Descricao:"R$30,00" 
-//     },
-//     {
-//         titulo:"The Force Unleashed"
-//         Descricao:"R$10,00" 
-//     },
-//     {
-//         titulo:"The Sky Walker Saga"
-//         Descricao:"R$10,00" 
-//     },
-//     {
-//         titulo:"Star Wars III"
-//         Descricao:"R$10,00" 
-//     }
-//     {
-//         titulo:"Old Republic"
-//         Descricao:"R$10,00" 
-//     },
-// ]
 
-// function renderizarProdutos(){
-//     let espaco = document.getElementById("espaco-produtos")
+function renderizarProdutos() {
+    let espaco = document.getElementById("espaco-produtos")
 
-//     let template = "";
+    let template = "";
 
-//     for (let index = 0; index < listaProdutos.length; index++) {
-//         const produtos = listaProdutos[index];
+    for (let index = 0; index < listaProdutos.length; index++) {
+        const produtos = listaProdutos[index];
 
-//         template +=` <div class="produto1 col-md-3">
-//         <img src="Img/Produto 1.png" alt="Jogo God of War"> 
-//         <h3>${produtos.titulo}</h3>
-//         <p>${produtos.Descricao}</p>  
-//         <a href="">Comprar</a>
-//     </div>`        
-//     }
-//     espaco.innerHTML = template;
-// }
+        template +=` <div class="produtos col-md-3">
+        <img src="Img/Produto 1.png" alt="Jogo God of War"> 
+        <h3>${produtos.titulo}</h3>
+        <p>${produtos.Descricao}</p>  
+        <a href="">Comprar</a>
+    </div>`        
+    }
+    
+    espaco.innerHTML = template;
+}
