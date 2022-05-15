acec75ce91da9ac1b0ae7fe4ec87679576d01f74
karensand <ksilva@mtsolucoes.com.br>
2022-05-15 11:08:16 -0300

    Ajustes dia15.05

--- SA2 - Game Mania/js/script.js
+++ SA2 - Game Mania/js/script.js
@@ -43,72 +43,72 @@ let email = document.getElementById("campo-email");
 //     console.log(emailDigitado)
 // }
 
-let listaProdutos = [
-    {
-        titulo: "God of War",
-        descricao: "R$10,00"
-    },
-    {
-        titulo: "PSE 2021",
-        descricao: "R$15,00" 
-    },
-    {
-        titulo: "Fida 2022",
-        descricao: "R$20,00" 
-    },
-    {
-        titulo: "Nabunagasr",
-        descricao: "R$10,00" 
-    },
-    {
-        titulo: "Kit teclado",
-        descricao: "R$150,00" 
-    },
-    {
-        titulo: "Acessorios",
-        descricao: "R$100,00" 
-    },
-    {
-        titulo: "Cadeira",
-        descricao: "R$350,00" 
-    },
-    {
-        titulo: "Suporte para celula",
-        descricao: "R$50,00" 
-    },
-    {
-        titulo: "Assasinos Creed Valhalla",
-        descricao: "R$10,00" 
-    },
-    {
-        titulo: "Assasinos Creed III",
-        descricao: "R$10,00" 
-    },
-    {
-        titulo: "The Witcher",
-        descricao: "R$25,00" 
-    },
-    {
-        titulo: "Velozes e Furiosos Encruzilhada",
-        descricao: "R$30,00" 
-    },
-    {
-        titulo: "The Force Unleashed",
-        descricao: "R$10,00" 
-    },
-    {
-        titulo: "The Sky Walker Saga",
-        descricao: "R$10,00" 
-    },
-    {
-        titulo: "Star Wars III",
-        descricao: "R$10,00" 
-    },
-    {
-        titulo: "Old Republic",
-        descricao: "R$10,00" 
-    },
-]
+// let listaDestaque = [
+//     {
+//         titulo: "God of War",
+//         descricao: "R$10,00"
+//     },
+//     {
+//         titulo: "PSE 2021",
+//         descricao: "R$15,00" 
+//     },
+//     {
+//         titulo: "Fida 2022",
+//         descricao: "R$20,00" 
+//     },
+//     {
+//         titulo: "Nabunagasr",
+//         descricao: "R$10,00" 
+//     },
+//     {
+//         titulo: "Kit teclado",
+//         descricao: "R$150,00" 
+//     },
+//     {
+//         titulo: "Acessorios",
+//         descricao: "R$100,00" 
+//     },
+//     {
+//         titulo: "Cadeira",
+//         descricao: "R$350,00" 
+//     },
+//     {
+//         titulo: "Suporte para celula",
+//         descricao: "R$50,00" 
+//     },
+//     {
+//         titulo: "Assasinos Creed Valhalla",
+//         descricao: "R$10,00" 
+//     },
+//     {
+//         titulo: "Assasinos Creed III",
+//         descricao: "R$10,00" 
+//     },
+//     {
+//         titulo: "The Witcher",
+//         descricao: "R$25,00" 
+//     },
+//     {
+//         titulo: "Velozes e Furiosos Encruzilhada",
+//         descricao: "R$30,00" 
+//     },
+//     {
+//         titulo: "The Force Unleashed",
+//         descricao: "R$10,00" 
+//     },
+//     {
+//         titulo: "The Sky Walker Saga",
+//         descricao: "R$10,00" 
+//     },
+//     {
+//         titulo: "Star Wars III",
+//         descricao: "R$10,00" 
+//     },
+//     {
+//         titulo: "Old Republic",
+//         descricao: "R$10,00" 
+//     },
+// ]
 
 function enviarEmail() {
     let emailDigitado = email.value;
@@ -116,21 +116,21 @@ function enviarEmail() {
 }
 
 
-function renderizarProdutos() {
-    let espaco = document.getElementById("espaco-produtos")
+// function renderizarDestaque() {
+//     let espaco = document.getElementById("espaco-destaque")
 
-    let template = "";
+//     let template = "";
 
-    for (let index = 0; index < listaProdutos.length; index++) {
-        const produtos = listaProdutos[index];
+//     for (let index = 0; index < listaDestaque.length; index++) {
+//         const destaque = listaDestaque[index];
 
-        template +=` <div class="produtos col-md-3">
-        <img src="Img/Produto 1.png" alt="Jogo God of War"> 
-        <h3>${produtos.titulo}</h3>
-        <p>${produtos.Descricao}</p>  
-        <a href="">Comprar</a>
-    </div>`        
-    }
+//         template +=` <div class="produto1 col-md-3">
+//         <img src="Img/Produto 1.png" alt="Jogo God of War"> 
+//         <h3>${destaque.titulo}</h3>
+//         <p>${destaque.descricao}</p>  
+//         <a href="">Comprar</a>
+//     </div>`        
+//     }
     
-    espaco.innerHTML = template;
-}
+//     espaco.innerHTML = template;
+// }
